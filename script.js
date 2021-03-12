0ab50e10818dc019e567b0c05eb19cb1dcc6004c
HitenG2 <77940279+HitenG2@users.noreply.github.com>
2021-03-12 23:12:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function substr(word, begin, n){
+  //if (begin > word.length || n > word.length) console.log("Wrong arguments ('begin' or 'n' can't be more, than length of word)");
+  //else if (begin + n > word.length) console.log("Wrong arguments ('begin' + 'n' more, than length of word)");
+  try{
+    let cuttedWord = '';
+    for (let i = begin; i != begin + n; ++i){
+      cuttedWord += word[i];
+    }
+    return cuttedWord;
+  }
+  catch(err){
+    console.error(err);
+  }
+
+}
+
+function isPalindrome(word){
+  if (word.length <= 1) return true;
+  let length = word.length;
+  if (word[length - length] == word[length - 1]) return isPalindrome(substr(word, length - length + 1, length - 2));
+  else return false;
+}
+
+function apply(n, func, value){
+  for (let i = 0; i < n; ++i){
+    value = func(value);
+  }
+  return value;
+}
